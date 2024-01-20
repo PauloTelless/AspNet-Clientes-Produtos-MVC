f64fdcf1909a6cbc9ef4a887d071612c66b91079
Paulo Telles <ptelles020@gmail.com>
2024-01-19 23:39:58 -0300

    atualização da exibição do preço/formatação para R$

--- ClientesProdutosOracleMVC/Repositories/ProdutoRepository/ProdutoRepository.cs
+++ ClientesProdutosOracleMVC/Repositories/ProdutoRepository/ProdutoRepository.cs
@@ -6,18 +6,25 @@ namespace ClientesProdutosOracleMVC.Repositories.ProdutoRepository;
 
 public class ProdutoRepository : IProduto
 {
+    private readonly ICliente _clienteRepositorio;
     private readonly AppDbContext _context;
-    public ProdutoRepository(AppDbContext context)
+    public ProdutoRepository(AppDbContext context, ICliente clienteRepositorio)
     {
         _context = context;
+        _clienteRepositorio = clienteRepositorio;
     }
-
     public IEnumerable<Produto> ObterProdutosDoCliente(int id)
     {
         return _context.Produtos
             .Where(x => x.ClienteId == id)
             .ToList();
     }
+    public Produto AdicionarProduto(int clienteid, Produto produto)
+    {
+        var cliente = _clienteRepositorio.EncontrarIdCliente(clienteid);
 
+        var id = _context.Produtos.Where(x => x.ClienteId == cliente.ClienteId);
 
+        return produto;
+    }
 }
